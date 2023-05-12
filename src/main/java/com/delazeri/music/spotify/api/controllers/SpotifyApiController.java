16b4398b482d5d0700c7a3d8b65dc6d0a8ccb560
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-05-12 15:30:46 -0300

    feat: populate database by spotify album id

--- src/main/java/com/delazeri/music/spotify/api/controllers/SpotifyApiController.java
+++ src/main/java/com/delazeri/music/spotify/api/controllers/SpotifyApiController.java
@@ -1,21 +1,22 @@
 package com.delazeri.music.spotify.api.controllers;
 
-import com.delazeri.music.spotify.api.entities.Playlist;
-import com.delazeri.music.spotify.api.services.SpotifyApiService;
+import com.delazeri.music.albums.entities.Album;
+import com.delazeri.music.spotify.api.services.SpotifyAlbumService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("populate")
 public class SpotifyApiController {
 
     @Autowired
-    SpotifyApiService spotifyApiService;
+    SpotifyAlbumService spotifyApiService;
 
-    @GetMapping("by-playlist/{playlistId}")
-    public ResponseEntity<Playlist> populateByPlaylist(@PathVariable String playlistId, @RequestHeader("Authorization") String accessToken) throws JsonProcessingException {
-        return ResponseEntity.ok().body(spotifyApiService.getPlaylistTracks(playlistId, accessToken));
+    @GetMapping("by-album/{albumId}")
+    public ResponseEntity<Album> populateByAlbum(@PathVariable String albumId, @RequestHeader("Authorization") String accessToken) throws JsonProcessingException {
+        return ResponseEntity.ok().body(spotifyApiService.populateDatabaseBySpotifyAlbum(albumId, accessToken));
     }
 }
