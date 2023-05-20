f6282be2992abc0c303c18877f577f4ad9896870
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-05-19 23:42:24 -0300

    feat: setup cors globally

--- src/main/java/com/delazeri/music/albums/controllers/AlbumController.java
+++ src/main/java/com/delazeri/music/albums/controllers/AlbumController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin
 @RestController
 @RequestMapping("albums")
 public class AlbumController {
