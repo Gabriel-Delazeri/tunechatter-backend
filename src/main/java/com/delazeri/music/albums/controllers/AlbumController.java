f423c5442c5cfde333bf6209a9985d77c0f0cde2
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-05-20 00:59:50 -0300

    feat: replacing Album entity to Album DTO

--- src/main/java/com/delazeri/music/albums/controllers/AlbumController.java
+++ src/main/java/com/delazeri/music/albums/controllers/AlbumController.java
@@ -1,7 +1,7 @@
 package com.delazeri.music.albums.controllers;
 
 import com.delazeri.music.albums.dtos.AlbumDTO;
-import com.delazeri.music.albums.entities.Album;
+import com.delazeri.music.albums.dtos.SimpleAlbumDTO;
 import com.delazeri.music.albums.services.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,17 +18,17 @@ public class AlbumController {
     AlbumService service;
 
     @GetMapping
-    public ResponseEntity<List<AlbumDTO>> findAll() {
+    public ResponseEntity<List<SimpleAlbumDTO>> findAll() {
         return ResponseEntity.ok().body(service.findAll());
     }
 
     @GetMapping("by-id/{id}")
-    public ResponseEntity<Album> findById(@PathVariable UUID id) {
+    public ResponseEntity<AlbumDTO> findById(@PathVariable UUID id) {
         return ResponseEntity.ok().body(service.findById(id));
     }
 
     @GetMapping("by-slug/{slug}")
-    public ResponseEntity<Album> findBySlug(@PathVariable String slug) {
+    public ResponseEntity<AlbumDTO> findBySlug(@PathVariable String slug) {
         return ResponseEntity.ok().body(service.findBySlug(slug));
     }
 }
