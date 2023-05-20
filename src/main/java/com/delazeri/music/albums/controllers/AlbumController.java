19d37b695d38c4068e73df69ca75c4d652ad42c6
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-05-19 23:26:56 -0300

    feat: find album by slug

--- src/main/java/com/delazeri/music/albums/controllers/AlbumController.java
+++ src/main/java/com/delazeri/music/albums/controllers/AlbumController.java
@@ -1,5 +1,6 @@
 package com.delazeri.music.albums.controllers;
 
+import com.delazeri.music.albums.dtos.AlbumDTO;
 import com.delazeri.music.albums.entities.Album;
 import com.delazeri.music.albums.services.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +19,17 @@ public class AlbumController {
     AlbumService service;
 
     @GetMapping
-    public ResponseEntity<List<Album>> findAll() {
+    public ResponseEntity<List<AlbumDTO>> findAll() {
         return ResponseEntity.ok().body(service.findAll());
     }
 
-    @GetMapping("{id}")
+    @GetMapping("by-id/{id}")
     public ResponseEntity<Album> findById(@PathVariable UUID id) {
         return ResponseEntity.ok().body(service.findById(id));
+    }
+
+    @GetMapping("by-slug/{slug}")
+    public ResponseEntity<Album> findBySlug(@PathVariable String slug) {
+        return ResponseEntity.ok().body(service.findBySlug(slug));
     }
 }
--- src/main/java/com/delazeri/music/albums/services/AlbumService.java
+++ src/main/java/com/delazeri/music/albums/services/AlbumService.java
@@ -1,13 +1,12 @@
 package com.delazeri.music.albums.services;
 
+import com.delazeri.music.albums.dtos.AlbumDTO;
 import com.delazeri.music.albums.entities.Album;
 import com.delazeri.music.albums.repositories.AlbumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class AlbumService {
@@ -15,11 +14,20 @@ public class AlbumService {
     @Autowired
     AlbumRepository repository;
 
-    public List<Album> findAll() {
-        return repository.findAll();
+    public List<AlbumDTO> findAll() {
+        List<AlbumDTO> albums = repository.findAllAlbums();
+        albums.forEach(
+                albumDTO -> {albumDTO.setArtists(repository.findAlbumArtists(albumDTO.getId()));}
+        );
+
+        return albums;
     }
 
     public Album findById(UUID id) {
         return repository.findById(id).orElseThrow();
+    }
+
+    public Album findBySlug(String slug) {
+        return repository.findBySlug(slug).orElseThrow();
     }
 }
