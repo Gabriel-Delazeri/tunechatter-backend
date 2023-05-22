3ab58f51d5bd67015d35d6bdd0d6bb3c7873e26d
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-05-21 23:55:16 -0300

    fix: find artist of album query

--- src/main/java/com/delazeri/music/albums/repositories/AlbumRepository.java
+++ src/main/java/com/delazeri/music/albums/repositories/AlbumRepository.java
@@ -2,7 +2,7 @@ package com.delazeri.music.albums.repositories;
 
 import com.delazeri.music.albums.dtos.SimpleAlbumDTO;
 import com.delazeri.music.albums.entities.Album;
-import com.delazeri.music.artists.dtos.ArtistDTO;
+import com.delazeri.music.artists.entities.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,8 +16,8 @@ public interface AlbumRepository extends JpaRepository<Album, UUID> {
     @Query("SELECT new com.delazeri.music.albums.dtos.SimpleAlbumDTO(a.id, a.spotifyId, a.name, a.slug, a.imageUrl) FROM Album a order by a.name DESC ")
     List<SimpleAlbumDTO> findAllAlbums();
 
-    @Query("SELECT new com.delazeri.music.artists.dtos.ArtistDTO(a.id, a.spotifyId, a.externalUrl, a.name, a.slug) FROM Album a WHERE a.id = :albumId")
-    Set<ArtistDTO> findAlbumArtists(@Param("albumId") UUID albumId);
+    @Query("SELECT a.artists FROM Album a WHERE a.id = :albumId")
+    Set<Artist> findArtistsByAlbumId(@Param("albumId") UUID albumId);
 
     Optional<Album> findBySpotifyId(String spotifyId);
 
