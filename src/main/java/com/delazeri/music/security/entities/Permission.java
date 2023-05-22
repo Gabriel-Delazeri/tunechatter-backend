b3a3bd9a67949e216eee5a87275acd82e1822f7b
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-05-22 00:19:43 -0300

    feat: User and Permission entities implementing security interfaces

---
+++ src/main/java/com/delazeri/music/security/entities/Permission.java
@@ -0,0 +1,61 @@
+package com.delazeri.music.security.entities;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "permission")
+public class Permission implements GrantedAuthority, Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column
+    private String description;
+
+    public Permission() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Permission that = (Permission) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.description;
+    }
+}
