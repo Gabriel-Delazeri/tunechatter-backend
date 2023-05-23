9552500ecaf6dc12b43d9aa208cd1efea5c623ac
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-05-23 00:11:04 -0300

    feat: sign up basic

---
+++ src/main/java/com/delazeri/music/security/dtos/SignUpCredentialsDTO.java
@@ -0,0 +1,23 @@
+package com.delazeri.music.security.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignUpCredentialsDTO {
+
+    @NotBlank
+    private String userName;
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+}
