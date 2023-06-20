2dc99b7c48bf0066bf2c0a2c47449cb440dc0ca2
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-06-19 23:45:56 -0300

    feat: passing the release date in the albumsummary dto

--- src/main/java/com/delazeri/music/albums/dtos/AlbumSummaryDTO.java
+++ src/main/java/com/delazeri/music/albums/dtos/AlbumSummaryDTO.java
@@ -1,9 +1,11 @@
 package com.delazeri.music.albums.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Data
@@ -14,5 +16,8 @@ public class AlbumSummaryDTO {
     private UUID id;
     private String name;
     private String slug;
+    @JsonProperty(value = "image_url")
     private String imageUrl;
+    @JsonProperty(value = "release_date")
+    private LocalDate releaseDate;
 }
