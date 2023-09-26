acd8ce1402ca3de9706e42bff4549ef8af7d97fa
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-09-26 11:19:23 -0300

    fix: WebConfig

--- src/main/java/com/delazeri/music/infra/WebConfig.java
+++ src/main/java/com/delazeri/music/infra/WebConfig.java
@@ -1,6 +1,5 @@
 package com.delazeri.music.infra;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,13 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${cors.originPatterns:http://localhost:4200}")
-    private String corsOriginsPatterns = "";
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        String corsOriginsPatterns = "${cors.originPatterns:http://localhost:4200}";
+
         registry.addMapping("/**")
-                .allowedOrigins(this.corsOriginsPatterns)
+                .allowedOrigins(corsOriginsPatterns)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
